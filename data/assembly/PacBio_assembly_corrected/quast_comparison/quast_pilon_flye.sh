1226766cf12a8b09ceaed3fd0cfca018a0c2290d
Jinyi Li <jili2251@rackham1.uppmax.uu.se>
2024-05-07 18:47:11 +0200

    Compare the assembly quality between pilon correction and flye assembly

---
+++ data/assembly/PacBio_assembly_corrected/quast_comparison/quast_pilon_flye.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 8
+#SBATCH --time=72:00:00
+#SBATCH -J assembly_comparison
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user jinyi.li.2251@student.uu.se
+#SBATCH --output=assembly_comparison.%j.out
+#SBATCH --error=assembly_comparison.%j.err
+
+module load bioinfo-tools
+module load quast
+
+# Paths to the assembly files
+flye_assembly="/home/jili2251/genome_analysis_project/data/assembly/PacBio_assembly/assembly.fasta"
+pilon_assembly="/home/jili2251/genome_analysis_project/data/assembly/PacBio_assembly_corrrected/corrected_assembly.fasta"
+
+# Output directory for QUAST results
+output_dir="/home/jili2251/genome_analysis_project/data/assembly/PacBio_assembly_corrrected/quast_comparison"
+
+
+# Run QUAST
+quast.py -o ${output_dir} \
+         -l "Flye,Pilon" \
+         ${flye_assembly} \
+         ${pilon_assembly}
+
