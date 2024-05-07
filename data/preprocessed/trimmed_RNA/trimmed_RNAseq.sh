0b8bc718fd81f1097fd7c22cb9562077ecf18ab3
Jinyi Li <jili2251@rackham1.uppmax.uu.se>
2024-05-07 18:49:07 +0200

    Trim one of the RNA-seq dataset

---
+++ data/preprocessed/trimmed_RNA/trimmed_RNAseq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH --time=5:00:00
+#SBATCH -J trimmomatic_RNASeq_job
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user jinyi.li.2251@student.uu.se
+#SBATCH --output=trimmomatic_job.%j.out
+
+module load bioinfo-tools
+module load trimmomatic/0.39 
+
+# Running Trimmomatic for paired-end RNA-seq data trimming
+java -jar $TRIMMOMATIC_HOME/trimmomatic-0.39.jar PE \
+     -threads 4 \
+     /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/transcriptome/untrimmed/SRR6040095_scaffold_06.1.fastq.gz \
+     /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/transcriptome/untrimmed/SRR6040095_scaffold_06.2.fastq.gz \
+     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.1P.trimmed.fastq.gz \
+     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.1P.unpaired.fastq.gz \
+     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.2P.trimmed.fastq.gz \
+     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.2P.unpaired.fastq.gz \
+     ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE-2.fa:2:30:10 \
+     LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
