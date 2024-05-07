472c7ec5b63a776cb349fa67ffa76e4aae924f5d
Jinyi Li <jili2251@rackham1.uppmax.uu.se>
2024-05-07 19:32:31 +0200

    convert to paf file

---
+++ data/highergrade/assembly2/minimap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 8
+#SBATCH --time=72:00:00 
+#SBATCH -J miniasm_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user jinyi.li.2251@student.uu.se
+#SBATCH --output=miniasm_assembly.%j.out
+#SBATCH --error=miniasm_assembly.%j.err
+
+
+module load bioinfo-tools
+module load Miniasm/0.3-r179-20191007-ce615d1
+module load minimap2
+
+
+minimap2 -x ava-pb -t4 /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_06.fq.gz /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_06.fq.gz > reads.paf
+
+miniasm -f /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_06.fq.gz reads.paf > /home/jili2251/genome_analysis_project/data/highergrade/assembly2/assembly.gfa
+
+awk '/^S/{print ">"$2"\n"$3}' /home/jili2251/genome_analysis_project/data/highergrade/assembly2/assembly.gfa > /home/jili2251/genome_analysis_project/data/highergrade/assembly2/assembly.fasta
