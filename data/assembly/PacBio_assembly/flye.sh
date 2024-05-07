41f744c579a5c6590d79027d23bbe46248ae8d93
Jinyi Li <jili2251@rackham1.uppmax.uu.se>
2024-05-07 18:39:48 +0200

    First assmebly method for Pacbio sequencing data

---
+++ data/assembly/PacBio_assembly/flye.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH --time=12:00:00 
+#SBATCH -J flye_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user jinyi.li.2251@student.uu.se
+#SBATCH --output=flye_assembly.%j.out
+#SBATCH --output=flye_assembly.%j.err
+
+
+module load bioinfo-tools
+module load Flye/2.9.1
+
+flye --pacbio-raw /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_06.fq.gz \
+--out-dir /home/jili2251/genome_analysis_project/data/assembly/PacBio_assembly \
+--genome-size 27.36g \
+--threads 4 
+
+
