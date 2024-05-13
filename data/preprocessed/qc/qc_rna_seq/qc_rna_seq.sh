6866e0d028db5a6dd5693983fae8de8242f6e0d8
Jinyi Li <jili2251@rackham1.uppmax.uu.se>
2024-05-13 15:05:37 +0200

    qc for trimmed and untrimmed RNA seq file

---
+++ data/preprocessed/qc/qc_rna_seq/qc_rna_seq.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH --time=5:00:00
+#SBATCH -J FastQC_RNAseq_analysis
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user jinyi.li.2251@student.uu.se
+#SBATCH --output=FastQC_RNAseq_job.%j.out
+
+module load bioinfo-tools
+module load FastQC/0.11.9  # Check for the correct version of FastQC available
+
+# Paths to the untrimmed and trimmed RNA-seq data
+UNTRIMMED_PATH="/proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/transcriptome/untrimmed"
+TRIMMED_PATH="/home/jili2251/gene_pro/data/preprocessed/trimmed_RNA"
+
+# Output directory for FastQC results
+FASTQC_PATH="/home/jili2251/gene_pro/data/preprocessed/qc/qc_rna_seq"
+
+# Run FastQC on untrimmed RNA-seq data
+fastqc -o $FASTQC_PATH -t 4 \
+  $UNTRIMMED_PATH/SRR6040095_scaffold_06.1.fastq.gz \
+  $UNTRIMMED_PATH/SRR6040095_scaffold_06.2.fastq.gz
+
+# Run FastQC on trimmed RNA-seq data
+fastqc -o $FASTQC_PATH -t 4 \
+  $TRIMMED_PATH/SRR6040095_scaffold_06.1P.trimmed.fastq.gz \
+  $TRIMMED_PATH/SRR6040095_scaffold_06.2P.trimmed.fastq.gz
+
