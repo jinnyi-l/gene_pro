fe35186d101a45094858f33e9913b6842744585b
jinnyi-l <143734477+jinnyi-l@users.noreply.github.com>
2024-05-14 21:59:18 +0200

    Update flye.sh

--- data/assembly/PacBio_assembly/flye.sh
+++ data/assembly/PacBio_assembly/flye.sh
@@ -15,7 +15,7 @@ module load bioinfo-tools
 module load Flye/2.9.1
 
 flye --pacbio-raw /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_06.fq.gz \
---out-dir /home/jili2251/genome_analysis_project/data/assembly/PacBio_assembly \
+--out-dir /home/jili2251/gene_pro/data/assembly/PacBio_assembly \
 --genome-size 27.36g \
 --threads 4 
 
