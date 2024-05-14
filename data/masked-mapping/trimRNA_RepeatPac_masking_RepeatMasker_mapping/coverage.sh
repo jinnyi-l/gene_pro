ad3e704f28d5fad2122105b4dffa30b62c3d2d45
jinnyi-l <143734477+jinnyi-l@users.noreply.github.com>
2024-05-14 22:03:38 +0200

    Update coverage.sh

--- data/masked-mapping/trimRNA_RepeatPac_masking_RepeatMasker_mapping/coverage.sh
+++ data/masked-mapping/trimRNA_RepeatPac_masking_RepeatMasker_mapping/coverage.sh
@@ -16,19 +16,14 @@ module load samtools
 OUTPUT_DIR="/home/jili2251/gene_pro/data/masked-mapping/trimRNA_RepeatPac_masking_RepeatMasker_mapping"
 COVERAGE_FILE="${OUTPUT_DIR}/coverage.txt"
 
-# Header for the coverage file
 echo -e "Sample\tAverage Coverage" > $COVERAGE_FILE
 
-# Loop through BAM files in the directory and compute coverage
 for BAM_FILE in ${OUTPUT_DIR}/*.bam; do
     if [ -f "$BAM_FILE" ]; then
-        # Extract the sample name from the BAM file
         SAMPLE_NAME=$(basename $BAM_FILE ".bam")
-        
-        # Calculate the average coverage using samtools depth
+    
         AVG_COV=$(samtools depth -a $BAM_FILE | awk '{sum+=$3} END {if (NR>0) print sum/NR; else print "NA"}')
         
-        # Write the sample name and average coverage to the coverage file
         echo -e "${SAMPLE_NAME}\t${AVG_COV}" >> $COVERAGE_FILE
     fi
 done
