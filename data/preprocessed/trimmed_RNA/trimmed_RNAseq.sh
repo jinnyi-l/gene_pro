b6b27e871fbeebe40b6ad92ca8f943139690621a
jinnyi-l <143734477+jinnyi-l@users.noreply.github.com>
2024-05-14 22:09:03 +0200

    Update trimmed_RNAseq.sh

--- data/preprocessed/trimmed_RNA/trimmed_RNAseq.sh
+++ data/preprocessed/trimmed_RNA/trimmed_RNAseq.sh
@@ -12,15 +12,14 @@
 module load bioinfo-tools
 module load trimmomatic/0.39 
 
-# Running Trimmomatic for paired-end RNA-seq data trimming
 java -jar $TRIMMOMATIC_HOME/trimmomatic-0.39.jar PE \
      -threads 4 \
      /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/transcriptome/untrimmed/SRR6040095_scaffold_06.1.fastq.gz \
      /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/transcriptome/untrimmed/SRR6040095_scaffold_06.2.fastq.gz \
-     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.1P.trimmed.fastq.gz \
-     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.1P.unpaired.fastq.gz \
-     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.2P.trimmed.fastq.gz \
-     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.2P.unpaired.fastq.gz \
+     /home/jili2251/gene_pro/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.1P.trimmed.fastq.gz \
+     /home/jili2251/gene_pro/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.1P.unpaired.fastq.gz \
+     /home/jili2251/gene_pro/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.2P.trimmed.fastq.gz \
+     /home/jili2251/gene_pro/data/preprocessed/trimmed_RNA/SRR6040095_scaffold_06.2P.unpaired.fastq.gz \
      ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE-2.fa:2:30:10 \
      LEADING:20 TRAILING:20 SLIDINGWINDOW:4:15 MINLEN:36
 
