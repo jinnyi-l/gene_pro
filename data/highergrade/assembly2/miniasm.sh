ac89315f7fbb3fe62b7ac864b636a2122af603cd
jinnyi-l <143734477+jinnyi-l@users.noreply.github.com>
2024-05-14 22:00:33 +0200

    Update miniasm.sh

--- data/highergrade/assembly2/miniasm.sh
+++ data/highergrade/assembly2/miniasm.sh
@@ -16,15 +16,15 @@ module load miniasm/0.3-r179-20191007-ce615d1
 
 
 reads_path="/proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_06.fq.gz"
-overlap_file="/home/jili2251/genome_analysis_project/data/highergrade/assembly2/reads.paf"
+overlap_file="/home/jili2251/gene_pro/data/highergrade/assembly2/reads.paf"
 
 
 # Assemble using Miniasm
-miniasm -f $reads_path $overlap_file > /home/jili2251/genome_analysis_project/data/highergrade/assembly2/assembly.gfa
+miniasm -f $reads_path $overlap_file > /home/jili2251/gene_pro/data/highergrade/assembly2/assembly.gfa
 
 
 # Convert GFA to FASTA using awk
-awk '/^S/{print ">"$2"\n"$3}' /home/jili2251/genome_analysis_project/data/highergrade/assembly2/assembly.gfa > /home/jili2251/genome_analysis_project/data/highergrade/assembly2/assembly.fasta
+awk '/^S/{print ">"$2"\n"$3}' /home/jili2251/gene_pro/data/highergrade/assembly2/assembly.gfa > /home/jili2251/gene_pro/data/highergrade/assembly2/assembly.fasta
 
 
 
