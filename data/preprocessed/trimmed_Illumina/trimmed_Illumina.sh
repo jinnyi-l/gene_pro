9feed39fa745d617de282e7c2c41f66b23df302e
jinnyi-l <143734477+jinnyi-l@users.noreply.github.com>
2024-05-14 22:08:28 +0200

    Update trimmed_Illumina.sh

--- data/preprocessed/trimmed_Illumina/trimmed_Illumina.sh
+++ data/preprocessed/trimmed_Illumina/trimmed_Illumina.sh
@@ -16,9 +16,9 @@ java -jar $TRIMMOMATIC_HOME/trimmomatic-0.39.jar PE \
      -threads 4 \
      /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/illumina_data/SRR6058604_scaffold_06.1P.fastq.gz \
      /proj/uppmax2024-2-7/Genome_Analysis/4_Tean_Teh_2017/illumina_data/SRR6058604_scaffold_06.2P.fastq.gz \
-     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_Illumina/SRR6058604_scaffold_06.1P.trimmed.fastq.gz \
-     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_Illumina/SRR6058604_scaffold_06.1P.unpaired.fastq.gz \
-     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_Illumina/SRR6058604_scaffold_06.2P.trimmed.fastq.gz \
-     /home/jili2251/genome_analysis_project/data/preprocessed/trimmed_Illumina/SRR6058604_scaffold_06.2P.unpaired.fastq.gz \
+     /home/jili2251/gene_pro/data/preprocessed/trimmed_Illumina/SRR6058604_scaffold_06.1P.trimmed.fastq.gz \
+     /home/jili2251/gene_pro/data/preprocessed/trimmed_Illumina/SRR6058604_scaffold_06.1P.unpaired.fastq.gz \
+     /home/jili2251/gene_pro/data/preprocessed/trimmed_Illumina/SRR6058604_scaffold_06.2P.trimmed.fastq.gz \
+     /home/jili2251/gene_pro/data/preprocessed/trimmed_Illumina/SRR6058604_scaffold_06.2P.unpaired.fastq.gz \
      ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10 \
      LEADING:20 TRAILING:20 SLIDINGWINDOW:4:15 MINLEN:36
